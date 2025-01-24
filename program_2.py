d7a08a29178539cc88cdefd25f3c6f87148ae153
Lucijoy <ljfloan2@students.unwsp.edu>
2025-01-24 16:09:45 -0600

    Lucia Floan program_2.py 1/24/25

--- program_2.py
+++ program_2.py
@@ -1,7 +1,7 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
     print('"Do you see a man skillful in his work?"')
-    print('    He will stand before kings;') #indented by 4 spaces to center
+    print('    He will stand before kings;') # Indented by 4 spaces to center
     print('he will not stand before obscure men."')
     print('       (Proverbs 22:29)') # Indented by 7 spaces to center
     
